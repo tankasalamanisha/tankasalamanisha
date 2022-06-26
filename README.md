5c380eb63519c5d629b4d9e853b7aae9310cefea
tankasalamanisha <tankasalamanisha95@gmail.com>
2022-06-26 19:44:13 +0530

    Updated profile readme

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-- 👋 Hi, I’m @tankasalamanisha
-- 👀 I’m interested in Machine learning and Applied AI
-- 🌱 I’m currently learning Python,Data Structures, JSON,GeoJson, MongoDB ,QGIS,Linux OS, Quantum Computing and Image processing.I enjoy working in VS code and Vim.
-- 💞️ I’m looking to collaborate on AI-ML and OpenCV projects
-- 📫 How to reach me : linkedin- https://www.linkedin.com/in/manishatv/
-
-<!---
-tankasalamanisha/tankasalamanisha is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+Hi,
+My name is Manisha Akshita T V and today is Sunday, 26 June, 16:13 CEST.
---
+++ index.js
@@ -0,0 +1,36 @@
+//index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+/**
+ * DATA is the object that contains all the data to be provided to Mustache.
+ */
+let DATA = {
+	name: 'Manisha Akshita T V',
+	date: new Date().toLocaleDateString('en-GB',{
+		weekday: 'long',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		timeZoneName: 'short',
+		timeZone: 'Europe/Stockholm',
+	}),
+};
+
+/**
+ * A- Open 'main.mustache'
+ * B- Ask Mustache to render our file with data
+ * C- Create a README.md file with generated output
+ */
+
+function generateReadMe(){
+   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+	   if (err) throw err;
+	   const output = Mustache.render(data.toString(), DATA);
+	   fs.writeFileSync('README.md', output);
+   });
+}
+
+generateReadMe();
